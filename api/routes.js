b8c0a78a45ff73d0ed7abe32c43e04e0e3586111
tailung2908 <locmeng123@gmail.com>
2025-09-15 23:34:53 -0400

    Add basic API endpoints

--- api/routes.js
+++ api/routes.js
@@ -1,1 +1,19 @@
-s
+// API Routes for Users
+// -------------------
+// This file provides REST API endpoints for user operations
+
+const express = require('express');
+const router = express.Router();
+
+// GET all users
+router.get('/users', (req, res) => {
+    res.send('Return all users');
+});
+
+// GET user by ID
+router.get('/users/:id', (req, res) => {
+    res.send(`Return user with ID ${req.params.id}`);
+});
+
+module.exports = router;
+
