d0ab8c6871d5a41a6fc65458f30ce54ef76c74d9
tailung2908 <locmeng123@gmail.com>
2025-09-15 23:32:27 -0400

    Add basic database connection setup

--- database.js
+++ database.js
@@ -1,1 +1,23 @@
-s
+// Database connection module
+// --------------------------
+// This file connects to the MySQL database
+
+const mysql = require('mysql');
+
+// Create a connection object
+const connection = mysql.createConnection({
+  host: 'localhost',       // database host
+  user: 'root',            // database username
+  password: 'password',    // database password
+  database: 'mydb'         // database name
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database!');
+});
+
+// Export the connection
+module.exports = connection;
+
